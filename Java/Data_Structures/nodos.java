ab35c9d57ef6dec69fb788eea1b2c80baddf6d23
Brayan Riveros <alejandroriveros100@gmail.com>
2023-01-17 16:03:46 -0500

    [ADD] static class nodoAVL-TREE

--- Java/Data_Structures/nodos.java
+++ Java/Data_Structures/nodos.java
@@ -36,4 +36,50 @@ public class nodos{
             this.der=null;
         }
     }
+    public static class nodoArbolAVL<T>{
+        public int clave;
+        private int fb;//-->factor de balance
+        public T info;
+        public nodoArbolAVL<T> izq;
+        public nodoArbolAVL<T> der;
+        public nodoArbolAVL(int clave,T info){
+            this.clave=clave;
+            this.info=info;
+            this.izq=null;
+            this.der=null;
+        }
+        public nodoArbolAVL(int clave){
+            this.clave=clave;
+            this.info=null;
+            this.izq=null;
+            this.der=null;
+        }
+        public nodoArbolAVL(){
+            this.info=null;
+            this.izq=null;
+            this.der=null;
+        }
+        
+        public int getFb(){
+            fb=(this.der.altura()-this.izq.altura());
+            return this.fb;
+        }
+
+        private int altura(nodoArbolAVL<T> actual){
+            if (actual == null){
+                return 0;
+            }else {
+                /* calcular la altura de cada subarbol*/
+                int izqAltura = altura(actual.izq);
+                int derAltura = altura(actual.der);
+     
+                /* retornar el mas "alto" */
+                return (izqAltura > derAltura)?(izqAltura + 1):(derAltura + 1);
+            }
+        }
+
+        public int altura(){
+            return altura(this);
+        }
+    }
 }
