12e486a54bb5474e546e93bc02e74bf8734454e6
Brayan Riveros <alejandroriveros100@gmail.com>
2023-01-16 19:36:11 -0500

    [ADD] clase Nodo arbolBinario

--- Java/Data_Structures/nodos.java
+++ Java/Data_Structures/nodos.java
@@ -13,4 +13,27 @@ public class nodos{
             this.sig=null;
         }
     }
+    public static class nodoArbolBin<T>{
+        public int clave;
+        public T info;
+        public nodoArbolBin<T> izq;
+        public nodoArbolBin<T> der;
+        public nodoArbolBin(int clave,T info){
+            this.clave=clave;
+            this.info=info;
+            this.izq=null;
+            this.der=null;
+        }
+        public nodoArbolBin(int clave){
+            this.clave=clave;
+            this.info=null;
+            this.izq=null;
+            this.der=null;
+        }
+        public nodoArbolBin(){
+            this.info=null;
+            this.izq=null;
+            this.der=null;
+        }
+    }
 }
